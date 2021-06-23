27d0dfd07e8c483c597d20290cb98e981771009d
birkin <birkin.diana@gmail.com>
2021-06-23 06:38:28 -0400

    stubs root, version, and info endpoints.

--- gsheet_processor/src/main.rs
+++ gsheet_processor/src/main.rs
@@ -1,16 +1,4 @@
-// fn main() {
-//     println!("Hello, world!");
-// }
-
-
-
 #[macro_use] extern crate rocket;
-
-// #[get("/")]
-// fn index() -> &'static str {
-//     "Hello, world!"
-// }
-
 
 
 // see <https://github.com/SergioBenitez/Rocket/blob/v0.5-rc/examples/hello/src/main.rs>
@@ -19,19 +7,51 @@
 
 // in this section, get a route to '/info', and a route to '/version' to work
 
-
-
-// below works
-
 #[get("/")]
 async fn index() -> &'static str {
     use rocket::tokio::time::{sleep, Duration};
     sleep(Duration::from_secs(2)).await;
-    "Hello, world!"
+    "coming: root-response"
+}
+
+#[get("/version")]
+async fn version() -> &'static str {
+    use rocket::tokio::time::{sleep, Duration};
+    sleep(Duration::from_secs(2)).await;
+    "coming: version-response"
+}
+
+#[get("/info")]
+async fn info() -> &'static str {
+    use rocket::tokio::time::{sleep, Duration};
+    sleep(Duration::from_secs(2)).await;
+    "coming: info-response"
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
+        .mount("/", routes![version])
+        .mount("/", routes![info])
 }
+
+
+
+
+// works -- start
+
+// #[get("/")]
+// async fn index() -> &'static str {
+//     use rocket::tokio::time::{sleep, Duration};
+//     sleep(Duration::from_secs(2)).await;
+//     "Hello, world!"
+// }
+
+// #[launch]
+// fn rocket() -> _ {
+//     rocket::build()
+//         .mount("/", routes![index])
+// }
+
+// works -- end
